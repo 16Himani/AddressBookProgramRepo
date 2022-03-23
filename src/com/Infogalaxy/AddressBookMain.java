0bc88c1ce613047c7179885ddbc066fdf81a373d
16Himani <himanipatil016@gmail.com>
2022-03-23 13:42:31 +0530

    MODIFIED: Edit Existing Contact

--- src/com/Infogalaxy/AddressBookMain.java
+++ src/com/Infogalaxy/AddressBookMain.java
@@ -31,11 +31,37 @@ public class AddressBookMain {
         System.out.println(contact.toString());
 
     }
+
+    public void editContact(){
+        System.out.println("Enter the FirstName :");
+        String fname = sc.next();
+        if(fname.equals(contact.getFirstName())) {
+            System.out.println("Contact is Found..");
+            System.out.println("Enter the Last Name:");
+            contact.setLastName(sc.next());
+            System.out.println("Enter the Address:");
+            contact.setAddress(sc.next());
+            System.out.println("Enter the City:");
+            contact.setCity(sc.next());
+            System.out.println("Enter the State:");
+            contact.setState(sc.next());
+            System.out.println("Enter the zip:");
+            contact.setZip(sc.next());
+            System.out.println("Enter the Mobno:");
+            contact.setMobNo(sc.next());
+            System.out.println("Enter the Email");
+            contact.setEmail(sc.next());
+
+        } else {
+            System.out.println("Contact Not Found..");
+        }
+    }
     public static void main(String[] args) {
             AddressBookMain addressBookmain = new AddressBookMain();
             addressBookmain.addContact();
             addressBookmain.showContact();
-
+            addressBookmain.editContact();
+            addressBookmain.showContact();
 
     }
 }
