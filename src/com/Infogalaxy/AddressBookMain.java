f7fa026308147863981dbf8fd67454540069e7fc
16Himani <himanipatil016@gmail.com>
2022-03-23 13:57:12 +0530

    MODIFIED: Delete Contact

--- src/com/Infogalaxy/AddressBookMain.java
+++ src/com/Infogalaxy/AddressBookMain.java
@@ -28,7 +28,11 @@ public class AddressBookMain {
     }
 
     public void showContact(){
-        System.out.println(contact.toString());
+            try {
+                System.out.println(contact.toString());
+            }catch(NullPointerException npe){
+                System.out.println("Contact is Blank");
+            }
 
     }
 
@@ -56,12 +60,23 @@ public class AddressBookMain {
             System.out.println("Contact Not Found..");
         }
     }
+    public void deleteContact(){
+        System.out.println("Enter The First Name");
+        String fname = sc.next();
+        if(fname.equals(contact.getFirstName())){
+            System.out.println("Contact Found");
+            contact = null;
+        } else{
+            System.out.println("Contact Not Found");
+        }
+    }
     public static void main(String[] args) {
             AddressBookMain addressBookmain = new AddressBookMain();
             addressBookmain.addContact();
             addressBookmain.showContact();
             addressBookmain.editContact();
             addressBookmain.showContact();
-
+            addressBookmain.deleteContact();
+            addressBookmain.showContact();
     }
 }
